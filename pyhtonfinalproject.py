565d0cdead49a6c3b6494c76743f6803c73127d4
Seth <sethcastle@outlook.sg>
2025-12-09 17:13:42 +0800

    Update pyhtonfinalproject.py
    add color add error checking.

--- pyhtonfinalproject.py
+++ pyhtonfinalproject.py
@@ -2,14 +2,18 @@
 list = []
 quit = "N"
 while quit == "N":
-    print("To do list: ", list)
-    quit = input("Quit: Y/N ?: ").upper()
-    choice = input("What would you like to do?(add, remove): ")
+    print("\033[92mTo do list: ", list)
+    
+    choice = input("\033[0mWhat would you like to do?(add, remove): ")
     if choice == "add":
         new_task = input("Add new task: ")
         list.append(new_task)
         continue
     elif choice == "remove":
         removed_task = input("Remove task: ")
-        list.remove(removed_task)
+        if removed_task in list:
+            list.remove(removed_task)
+        else:
+            print("\033[31mTask does not exist!!!!\033[0m")
         continue
+    quit = input("Quit: Y/N ?: ").upper()
